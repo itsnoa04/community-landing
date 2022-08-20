96aef475d4c09cdce4f04618ec8d03685f03acec
Devamsh Manoj <itsdevamshmanoj@gmail.com>
2022-08-20 14:32:56 +0530

    banner - animations

--- components/sections/mid/banner.tsx
+++ components/sections/mid/banner.tsx
@@ -1,8 +1,18 @@
+import { useInView } from "react-intersection-observer";
 import DynamicBanner from "../../sample/dynamicBanner";
 
 const Banner = () => {
+  const { ref, inView } = useInView();
   return (
-    <div className="  text-white m-5 rounded-3xl overflow-hidden">
+    <div
+      className={`text-white m-5 rounded-3xl overflow-hidden -translate-y-full ${
+        !inView ? "opacity-0 " : "opacity-100 -translate-y-0"
+      }`}
+      ref={ref}
+      style={{
+        transition: "all 1s ease",
+      }}
+    >
       <DynamicBanner />
     </div>
   );
